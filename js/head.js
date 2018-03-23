34c4ea54e6147a1e7d66adf879f87f0f872ea310
Bruno Vavala <vavala@lasige.di.fc.ul.pt>
2018-03-22 23:49:46 -0700

    fixed icon

--- js/head.js
+++ js/head.js
@@ -1,12 +1,15 @@
-function seticon(filename) {
-	var fileref=document.createElement('link');
-	fileref.setAttribute("rel", "icon");
-        fileref.setAttribute("href", filename);
-}
+document.write("<link rel='icon' type='image/png' href='img/icons/vavala-logo.png' />");
 
-seticon("img/icons/vavala-logo.png");
+//function seticon(filename) {
+//	var fileref=document.createElement("link");
+//	fileref.setAttribute("rel", "icon");
+//        fileref.setAttribute("href", filename);
+//}
+
+seticon("vavala-logo.pngc");
 
 document.write("<title>*****</title>");
+
 
 //document.write("
 //<link rel='icon' href='./img/icons/vavala-logo.png'>
