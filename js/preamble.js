32914a5c29c9470ce4ffa206db99bfb7c716bc2a
Bruno Vavala <vavala@lasige.di.fc.ul.pt>
2018-03-22 22:28:59 -0700

    js's, fonts, etc.

--- js/preamble.js
+++ js/preamble.js
@@ -1,16 +1,16 @@
 document.getElementById("preamblejs").innerHTML =
 '	<!-- Scripts -->'+
-'	<script src="https://www.w3schools.com/lib/w3.js"></script>'+
-'	<script src="https://code.jquery.com/jquery-1.8.3.min.js"></script>'+
+'	<script src="js/w3.js"></script>'+
+'	<script src="js/jquery-1.8.3.min.js"></script>'+
 '	<script src="js/highlight-menu.js"></script>'+
 '	<!-- Styles -->'+
 '	<link href="./css/css.css" media="screen" rel="stylesheet" type="text/css">'+
 '	<link href="./css/bruno-main.css" media="screen" rel="stylesheet" type="text/css">'+
 '	<link href="./css/sidenav.css" media="screen" rel="stylesheet" type="text/css">'+
 '	<link href="./css/bibtex.css" media="screen" rel="stylesheet" type="text/css">'+
-'	<link href="https://fonts.googleapis.com/css?family=Finger Paint" rel="stylesheet">'+
-'	<link href="https://fonts.googleapis.com/css?family=Nunito Sans" rel="stylesheet">'+
-'	<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700" rel="stylesheet">'+
+'	<link href="./css/fingerpaint.css" rel="stylesheet">'+
+'	<link href="./css/nunito.css" rel="stylesheet">'+
+'	<link href="./css/opensanscondensed.css" rel="stylesheet">'+
 '	<meta http-equiv="X-UA-Compatible" content="IE=Edge">';
 
 
